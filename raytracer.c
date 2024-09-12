ce629728abce7466bc893a3b94eb976786bb81bb
Atjowt <elliot.skogsberg@gmail.com>
2024-09-12 12:00:16 +0200

    attempt to add refraction

--- raytracer.c
+++ raytracer.c
@@ -84,6 +84,13 @@ vec3 vec3_reflect(vec3 v, vec3 n) {
 	return vec3_sub(v, vec3_scale(n, 2.0 * vec3_dot(v, n)));
 }
 
+vec3 vec3_refract(vec3 uv, vec3 n, double etai_over_etat) {
+    double cos_theta = fmin(-vec3_dot(uv, n), 1.0);
+    vec3 r_out_perp = vec3_scale(vec3_add(uv, vec3_scale(n, cos_theta)), etai_over_etat);
+    vec3 r_out_parallel = vec3_scale(n, -sqrt(fabs(1.0 - vec3_lensqr(r_out_perp))));
+    return vec3_add(r_out_perp, r_out_parallel);
+}
+
 static const vec3 UP = { 0.0, 0.0, 1.0 };
 static const vec3 RIGHT = { 0.0, 0.0, -1.0 };
 static const vec3 FORWARD = { -1.0, 0.0, 0.0 };
@@ -91,6 +98,7 @@ static const vec3 FORWARD = { -1.0, 0.0, 0.0 };
 static const vec3 RED = { 1.0, 0.0, 0.0 };
 static const vec3 GREEN = { 0.0, 1.0, 0.0 };
 static const vec3 BLUE = { 0.0, 0.0, 1.0 };
+static const vec3 YELLOW = { 0.0, 0.0, 1.0 };
 static const vec3 BLACK = { 0.0, 0.0, 0.0 };
 static const vec3 WHITE = { 1.0, 1.0, 1.0 };
 
@@ -119,14 +127,32 @@ typedef struct {
 	vec3 normal;
 } Plane;
 
+typedef struct {
+	enum {
+		SHAPE_PLANE,
+		SHAPE_SPHERE,
+	} type;
+	union {
+		Plane plane;
+		Sphere sphere;
+	} value;
+} Shape;
 
 typedef struct {
-	vec3 base_color;
-	double transparency;
+	vec3 color;
+	double roughness;
+	double transparent;
 	double metallic;
+	double specular;
+	double emission;
+	double transmission;
+	double ior;
 } Material;
 
-typedef struct Object Object;
+typedef struct {
+	Shape shape;
+	Material material;
+} Object;
 
 typedef struct {
 	bool outside;
@@ -136,23 +162,12 @@ typedef struct {
 	const Object* object;
 } Hit;
 
-struct Object {
-	Material material;
-	void* data;
-	bool (*hit) (Ray ray, const Object* self, double t_min, double t_max, Hit* hit);
-};
-
 typedef struct {
 	Object* objects;
-	int n_objects;
+	int count;
 } World;
 
-bool ray_hit_object(Ray ray, const Object* object, double t_min, double t_max, Hit* hit) {
-	return object->hit(ray, object, t_min, t_max, hit);
-}
-
-bool ray_hit_plane(Ray ray, const Object* object, double t_min, double t_max, Hit* hit) {
-	Plane plane = *(Plane*)object->data;
+bool ray_hit_plane(Ray ray, Plane plane, double t_min, double t_max, Hit* hit) {
 	double n_dot_d = vec3_dot(plane.normal, ray.direction);
 	if (n_dot_d == 0.0) {
 		return false;
@@ -168,12 +183,11 @@ bool ray_hit_plane(Ray ray, const Object* object, double t_min, double t_max, Hi
 		hit->point = ray_at(ray, t);
 		hit->outside = (n_dot_d < 0.0);
 		hit->normal = hit->outside ? plane.normal : vec3_scale(plane.normal, -1.0);
-		hit->object = object;
 	}
 	return true;
 }
 
-bool ray_hit_sphere(Ray ray, const Object* object, double t_min, double t_max, Hit* hit) {
+bool ray_hit_sphere(Ray ray, Sphere sphere, double t_min, double t_max, Hit* hit) {
 	/*x^2 + y^2 + z^2 = r^2*/
 	/*x = a+t*dx*/
 	/*y = b+t*dy*/
@@ -181,7 +195,6 @@ bool ray_hit_sphere(Ray ray, const Object* object, double t_min, double t_max, H
 	/*a^2 + 2*a*t*dx + t^2*dx^2 + b^2 + 2*b*t*dy + t^2*dy^2 + c^2 + 2*c*t*dz + t^2*dz^2 = r^2*/
 	/*(dx^2+dy^2+dz^2)*t^2 + (2*a*dx + 2*b*dy + 2*c*dz)*t + a^2+b^2+c^2-r^2 = 0*/
 	/*solve for t*/
-	Sphere sphere = *(Sphere*)object->data;
 	double a = ray.origin.x - sphere.center.x;
 	double b = ray.origin.y - sphere.center.y;
 	double c = ray.origin.z - sphere.center.z;
@@ -215,27 +228,34 @@ bool ray_hit_sphere(Ray ray, const Object* object, double t_min, double t_max, H
 		hit->normal = vec3_scale(vec3_sub(hit->point, sphere.center), 1.0 / r);
 		hit->outside = (vec3_dot(vec3_scale(ray.direction, -1.0), hit->normal) >= 0.0);
 		hit->normal = hit->outside ? hit->normal : vec3_scale(hit->normal, -1.0);
-		hit->object = object;
 	}
 	return true;
 }
 
-bool ray_hit_world(Ray ray, World world, double t_min, double t_max, Hit* hit) {
+bool ray_hit_shape(Ray ray, Shape shape, double t_min, double t_max, Hit* hit) {
+	switch (shape.type) {
+		case SHAPE_PLANE: return ray_hit_plane(ray, shape.value.plane, t_min, t_max, hit);
+		case SHAPE_SPHERE: return ray_hit_sphere(ray, shape.value.sphere, t_min, t_max, hit);
+	}
+	return false;
+}
 
-	/*static const Plane ground = {*/
-	/*	.point = { 0.0, 0.0, -0.5 },*/
-	/*	.normal = UP,*/
-	/*};*/
-	/**/
-	/*static const Sphere sphere = {*/
-	/*	.center = { 0.0, 1.5, 0.0 },*/
-	/*	.radius = 0.5,*/
-	/*};*/
+bool ray_hit_object(Ray ray, const Object* restrict object, double t_min, double t_max, Hit* restrict hit) {
+	if (ray_hit_shape(ray, object->shape, t_min, t_max, hit)) {
+		if (hit != NULL) {
+			hit->object = object;
+		}
+		return true;
+	}
+	return false;
+}
+
+bool ray_hit_world(Ray ray, World world, double t_min, double t_max, Hit* hit) {
 
 	Hit hit_temp;
 	bool hit_anything = false;
 
-	for (int i = 0; i < world.n_objects; i++) {
+	for (int i = 0; i < world.count; i++) {
 		if (ray_hit_object(ray, &world.objects[i], t_min, t_max, &hit_temp)) {
 			hit_anything = true;
 			t_max = hit_temp.t;
@@ -249,21 +269,29 @@ bool ray_hit_world(Ray ray, World world, double t_min, double t_max, Hit* hit) {
 }
 
 vec3 get_skybox_color(Ray ray) {
-	const vec3 sun_dir = vec3_normalize((vec3) { -0.5, 0.5, -1.0 });
-	float sun_fac = fmax(0.0, vec3_dot(ray.direction, vec3_scale(sun_dir, -1.0)));
-	static const vec3 sun_color = { 0.9, 0.8, 0.8 };
 	static const vec3 sky_color = { 0.6, 0.7, 0.9 };
+	static const vec3 sun_color = { 0.9, 0.8, 0.8 };
+	/*static const vec3 sky_color = { 0.1, 0.1, 0.1 };*/
+	/*static const vec3 sun_color = { 1.0, 1.0, 1.0 };*/
+	vec3 sun_dir = vec3_normalize((vec3) { 0.0, -0.1, -1.0 });
+	double sun_fac = fmax(0.0, -vec3_dot(ray.direction, sun_dir));
 	return vec3_lerp(sky_color, sun_color, sun_fac);
 }
 
-vec3 material_color(Material material, Ray ray, Hit hit) {
-	/*return hit.outside ? BLUE : RED;*/
-	return material.base_color;
+// Schlick's approximation
+double get_reflectance(double cos_theta, double ior) {
+        double r = (1.0 - ior) / (1.0 + ior);
+        double r2 = r * r;
+        return r2 + (1.0 - r2) * pow((1.0 - cos_theta), 5);
 }
 
-Ray material_scatter(Material material, Ray incoming, Hit hit) {
+vec3 get_material_color(Material material, Ray ray, Hit hit) {
+	/*return hit.outside ? BLUE : RED;*/
+	return material.color;
+	return vec3_lerp(material.color, WHITE, material.emission);
+}
 
-	static const double diffuse_normal_bias = 0.25;
+Ray material_bounce_ray(Material material, Ray incoming, Hit hit) {
 
 	Ray outgoing;
 
@@ -272,7 +300,7 @@ Ray material_scatter(Material material, Ray incoming, Hit hit) {
 	// Diffuse
 	/*outgoing.direction = vec3_normalize(vec3_add(hit.normal, vec3_random()));*/
 	double theta = TAU * frand();
-	double phi = diffuse_normal_bias * TAU * frand();
+	double phi = material.roughness * 0.5 * TAU * frand();
 	outgoing.direction = vec3_from_angles(theta, phi);
 	if (vec3_dot(outgoing.direction, hit.normal) <= 0.0) {
 		outgoing.direction = vec3_scale(outgoing.direction, -1.0);
@@ -283,7 +311,22 @@ Ray material_scatter(Material material, Ray incoming, Hit hit) {
         outgoing.direction = vec3_lerp(outgoing.direction, reflected, material.metallic);
 
 	// Transparent
-        outgoing.direction = vec3_lerp(outgoing.direction, incoming.direction, material.transparency);
+        outgoing.direction = vec3_lerp(outgoing.direction, incoming.direction, material.transparent);
+
+	// Emission
+        outgoing.direction = vec3_lerp(outgoing.direction, hit.normal, material.emission);
+
+	// Transmission
+	double ior = hit.outside ? (1.0 / material.ior) : material.ior;
+	double cos_theta = fmin(-vec3_dot(incoming.direction, hit.normal), 1.0);
+	double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+	vec3 refracted;
+	if ((ior * sin_theta > 1.0) || (get_reflectance(cos_theta, ior) > frand())) {
+		refracted = vec3_reflect(incoming.direction, hit.normal);
+	} else {
+		refracted = vec3_refract(incoming.direction, hit.normal, ior);
+	}
+        outgoing.direction = vec3_lerp(outgoing.direction, refracted, material.transmission);
 
 	return outgoing;
 }
@@ -295,8 +338,8 @@ vec3 trace_ray(Ray ray, World world, int bounces) {
 	while (true) {
 		if (bounces > 0 && ray_hit_world(ray, world, t_epsilon, INFINITY, &hit)) {
 			Material material = hit.object->material;
-			ray_color = vec3_prod(ray_color, material_color(material, ray, hit));
-			ray = material_scatter(material, ray, hit);
+			ray_color = vec3_prod(ray_color, get_material_color(material, ray, hit));
+			ray = material_bounce_ray(material, ray, hit);
 			bounces--;
 		} else {
 			return vec3_prod(ray_color, get_skybox_color(ray));
@@ -323,66 +366,108 @@ int main(void) {
 	static const double aspect_ratio = image_width / (double)image_height;
 	static const double viewport_height = 1.0;
 	static const double viewport_width = viewport_height * aspect_ratio;
-	static const int max_bounces = 12;
-	static const int samples_per_pixel = 24;
+	static const int max_bounces = 8;
+	static const int samples_per_pixel = 16;
 	const double fov = radians_from_degrees(60.0);
 	const double focal_length = focal_length_from_fov(fov);
 	const vec3 eye_pos = { 0.0, -focal_length, 0.0 };
 
 	Object objects[] = {
 		(Object) {
-			.data = &(Sphere) {
-				.center = { -1.0, 1.5, 0.0 },
-				.radius = 0.5,
+			.shape = {
+				.type = SHAPE_PLANE,
+				.value.plane = {
+					.point = { 0.0, 0.0, -0.5 },
+					.normal = UP,
+				},
 			},
-			.hit = ray_hit_sphere,
 			.material = {
-				.base_color = { 0.8, 0.1, 0.1 },
-				.transparency = 0.0,
+				.color = { 0.5, 0.5, 0.5 },
+				.roughness = 0.5,
+				.metallic = 0.5,
+				.transparent = 0.0,
+				.emission = 0.0,
+				.transmission = 0.0,
+				.ior = 1.0,
+			},
+		},
+		(Object) {
+			.shape = {
+				.type = SHAPE_SPHERE,
+				.value.sphere = {
+					.center = { -1.1, 1.0, 0.0 },
+					.radius = 0.5,
+				},
+			},
+			.material = {
+				.color = { 1.0, 0.1, 0.1 },
+				.roughness = 0.1,
 				.metallic = 1.0,
+				.transparent = 0.0,
+				.emission = 0.0,
+				.transmission = 0.0,
+				.ior = 1.0,
 			},
 		},
 		(Object) {
-			.data = &(Sphere) {
-				.center = { 0.0, 1.5, 0.0 },
-				.radius = 0.25,
+			.shape = {
+				.type = SHAPE_SPHERE,
+				.value.sphere = {
+					.center = { 0.0, 1.0, 0.0 },
+					.radius = 0.5,
+				},
 			},
-			.hit = ray_hit_sphere,
 			.material = {
-				.base_color = { 0.1, 0.8, 0.1 },
-				.transparency = 0.0,
+				.color = { 1.0, 1.0, 1.0 },
+				.roughness = 0.0,
 				.metallic = 0.0,
+				.transparent = 0.0,
+				.emission = 0.0,
+				.transmission = 1.0,
+				.ior = 1.5,
 			},
 		},
 		(Object) {
-			.data = &(Sphere) {
-				.center = { 1.0, 1.5, 0.0 },
-				.radius = 0.5,
+			.shape = {
+				.type = SHAPE_SPHERE,
+				.value.sphere = {
+					.center = { 0.0, 1.0, 0.0 },
+					.radius = 0.4,
+				},
 			},
-			.hit = ray_hit_sphere,
 			.material = {
-				.base_color = { 0.1, 0.1, 0.8 },
-				.transparency = 0.0,
+				.color = { 1.0, 1.0, 1.0 },
+				.roughness = 0.0,
+				.metallic = 0.0,
+				.transparent = 0.0,
+				.emission = 0.0,
+				.transmission = 1.0,
+				.ior = 1.0 / 1.5,
+			},
+		},
+		(Object) {
+			.shape = {
+				.type = SHAPE_SPHERE,
+				.value.sphere = {
+					.center = { 1.1, 1.0, 0.0 },
+					.radius = 0.5,
+				},
+			},
+			.material = {
+				.color = { 0.1, 0.1, 1.0 },
+				.roughness = 0.1,
 				.metallic = 1.0,
-			},
-		},
-		(Object) {
-			.data = &(Plane) {
-				.point = { 0.0, 0.0, -0.5 },
-				.normal = UP,
-			},
-			.hit = ray_hit_plane,
-			.material = {
-				.base_color = { 0.5, 0.5, 0.5 },
-				.transparency = 0.0,
-				.metallic = 0.0,
+				.transparent = 0.0,
+				.emission = 0.0,
+				.transmission = 0.0,
+				.ior = 1.0,
 			},
 		},
 	};
 
-	const World world = {
+	World world = {
 		.objects = objects,
-		.n_objects = sizeof(objects) / sizeof(Object),
+		.count = sizeof(objects) / sizeof(Object),
 	};
 
 	srand(12345);
@@ -413,7 +498,7 @@ int main(void) {
 			int blue_byte = 255.999 * sqrt(pixel_color.z);
 			fprintf(image_file, "%d %d %d\n", red_byte, green_byte, blue_byte);
 		}
-		putchar('\n');
+		/*putchar('\n');*/
 	}
 	fclose(image_file);
 	return 0;
